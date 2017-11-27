ca8ef573089cb56e55a5641ba1a42718e9d638c4
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 15:14:30 +0100

    feat(week-02/day-01):introduce myself

---
+++ week-02/day-01/introducemyself/introducemyself/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace introducemyself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rikker Dániel");
+            Console.WriteLine("21");
+            Console.WriteLine("176 cm");
+            Console.ReadKey();
+        }
+    }
+}
