7351d63f5efd1388036a1cce95de55eec38d29b5
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 15:40:14 +0100

    feat(week-02/day-01):favorite number

---
+++ week-02/day-01/FavoriteNumber/FavoriteNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FavoriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FavoriteNumber = 11;
+            Console.WriteLine("My favorite number is : " + FavoriteNumber);
+            Console.ReadKey();
+
+        }
+    }
+}
