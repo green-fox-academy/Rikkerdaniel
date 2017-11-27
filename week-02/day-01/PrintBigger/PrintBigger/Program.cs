5f090a0813cf660477524315d0af0df1036da77d
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 17:02:52 +0100

    feat(week-02/day-01):print bigger

---
+++ week-02/day-01/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number");
+            int number1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me the second number");
+            int number2= Convert.ToInt32(Console.ReadLine());
+            if (number1 >number2 )
+            {
+                Console.WriteLine(" "+number1 +"  is bigger");
+            }
+            else if (number2 >number1 )
+            {
+                Console.WriteLine(" "+number2 +"  is bigger");
+            }
+            else
+            {
+                Console.WriteLine("You gave me the same numbers you idiot ! i said an other one");
+            }
+            Console.ReadKey();
+        }
+    }
+}
