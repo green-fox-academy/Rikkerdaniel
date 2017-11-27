42b30dff1490a97badfd2269b2fa69ca18555639
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 15:46:26 +0100

    feat(week-02/day-01):Value swap

---
+++ week-02/day-01/valueSwap/valueSwap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace valueSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            int c = 0;
+            c = b;
+            b = a;
+            a = c;
+            Console.WriteLine(a +" "+ b);
+            Console.ReadKey();
+        }
+    }
+}
