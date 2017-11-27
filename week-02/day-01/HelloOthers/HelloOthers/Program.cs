6b47ec2dc1b61912131c078a69d2edd061a3f9c4
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 15:10:44 +0100

    feat(day-01)Greatotthers

---
+++ week-02/day-01/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Misi");
+            Console.WriteLine("Hello Erik");
+            Console.WriteLine("Hello Tamás");
+            Console.WriteLine("Hello Zsuzska");
+            Console.ReadKey();
+        }
+    }
+}
