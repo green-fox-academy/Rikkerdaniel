54721537a62d0b886efa69bd0a8c98a884e07a15
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 17:09:43 +0100

    feat(week-02/day-01):Draw Squared

---
+++ week-02/day-01/DrawSquared/DrawSquared/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DrawSquared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number!");
+            int number= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("%%%%%");
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("%   %");
+            }
+            Console.WriteLine("%%%%%");
+            Console.ReadKey();
+        }
+    }
+}
