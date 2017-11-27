f39dc13658a2d90a56947c2e900925426a17150d
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 16:48:34 +0100

    feat(week-01/day-01):cuboid

---
+++ week-02/day-01/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+            Console.WriteLine("Give me side a of the cuboid");
+            double sidea = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Give me side b of the cuboid");
+            double sideb = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Give me side c of the cuboid");
+            double sidec = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Volume: " + sidea *sideb *sidec +"cubed");
+            Console.WriteLine("surface: " + (2*(sidea *sideb )+2*(sideb *sidec )+2*(sidec *sidea )) +"squared");
+            Console.ReadKey();
+        }
+    }
+}
