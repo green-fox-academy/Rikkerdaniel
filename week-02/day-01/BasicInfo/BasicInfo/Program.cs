9099a1095ea92ff6439a12712b9d203e89f8227e
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 16:06:00 +0100

    feat(week-02/day-01):Basic info

---
+++ week-02/day-01/BasicInfo/BasicInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Whether you are married or not as a boolean
+            string name = "Danka";
+            int age = 21;
+            double hight = 1.76;
+            bool married = false;
+            Console.WriteLine(name +" \n " + age+"\n " +hight+" \n" + married  );
+            Console.ReadKey();
+        }
+    }
+}
