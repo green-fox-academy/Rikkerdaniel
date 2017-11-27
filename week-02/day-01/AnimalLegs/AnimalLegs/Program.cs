641e9ad6e97a7f3a41d4e3da1afcabccb78ce119
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 16:54:33 +0100

    feat(week-02/day-01):animals legs

---
+++ week-02/day-01/AnimalLegs/AnimalLegs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnimalLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many chickens do you have ?");
+            int chickens = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now tell me how many pigs do you have ");
+            int pigs= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your animals has : " + ((chickens *2)+(pigs *4)));
+            Console.ReadKey();
+        }
+    }
+}
