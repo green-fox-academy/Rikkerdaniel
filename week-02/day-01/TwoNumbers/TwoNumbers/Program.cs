98a995bd942311786968d949819dfbe2ce4edd27
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 15:26:15 +0100

    feat(week-02/day-01):two numbers

---
+++ week-02/day-01/TwoNumbers/TwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number0 = 22;
+            int number1 = 13;
+            Console.WriteLine(number0 + number1);
+            Console.WriteLine(number0 - number1);
+            Console.WriteLine(number0 * number1);
+            Console.WriteLine(number0 /(decimal)number1);
+            Console.WriteLine(number0 % number1);
+            Console.WriteLine(number0 / number1);
+            Console.ReadKey();
+        }
+    }
+}
