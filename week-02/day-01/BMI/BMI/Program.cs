e3392edd3f080deeb1c0c03bfa10dac87cd11761
Rikkerdaniel <rik.danka@gmail.com>
2017-11-27 15:57:04 +0100

    feat(week-02/day-01):BMI calculator

---
+++ week-02/day-01/BMI/BMI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me your hight in meters");
+            Double hight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Thank you:) \n Now give me your weight in kg please");
+            Double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your BMI is :  " + weight /(hight *hight ));
+            Console.ReadKey();
+        }
+    }
+}
