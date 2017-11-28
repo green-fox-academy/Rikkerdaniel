9f0e7ecfaa5709e1ddf571d3c7936586e3524d77
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 14:53:08 +0100

    feat(week-02/day-02):Double array values

---
+++ week-02/day-02/ArrayDouble/ArrayDouble/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < ag.Length; i++)
+            {
+                ag[i] = ag[i] + ag[i];
+                Console.WriteLine(ag[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
