639518180cd63c800f977c70032b1be2d341c9df
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 13:50:38 +0100

    feat(week-02/day-02):Third Number

---
+++ week-02/day-02/Third/Third/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = { 4, 5, 6, 7 };
+            Console.WriteLine("third element is "+q[2]);
+            Console.ReadKey();
+        }
+    }
+}
