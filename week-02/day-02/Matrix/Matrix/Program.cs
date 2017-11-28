d470d78272bf5e0179e3d45f779392bf6e973b2c
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 14:45:33 +0100

    feat(week-02/day-02):matrix thing

---
+++ week-02/day-02/Matrix/Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4, 4]
+            {
+            {1,0,0,0 },
+            { 0,1,0,0},
+            { 0,0,1,0},
+            { 0,0,0,1}
+            };
+            thing(array);
+            Console.ReadKey();
+            
+            
+            
+        }
+        static void thing(int[,]ar)
+        {
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+                for (int j = 0; j < ar.GetLength (1); j++)
+                {
+                    Console.Write(ar[i,j]);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
