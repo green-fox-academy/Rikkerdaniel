37826844a598623132bbf39b3d9a775c7498da4e
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 11:20:06 +0100

    feat(week-02/day-02):doubling method

---
+++ week-02/day-02/Doubling/Doubling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = Doubling(123);
+            Console.WriteLine(ak);
+            Console.ReadKey();
+        }
+        static int Doubling(int ak)
+        {
+            return ak + ak;
+        }
+    }
+}
