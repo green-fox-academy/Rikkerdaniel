1678972115e54132c170a753f7352ddc22c66eb8
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 11:28:44 +0100

    feat(week-02/day-02):Greeting Method

---
+++ week-02/day-02/GreatingMethod/GreatingMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreatingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            Greet(al );
+            Console.ReadKey();
+
+
+        }
+        static void  Greet(string al)
+        {
+            Console.WriteLine("Greetings dear ,"+ al +"!");
+        }
+    }
+}
