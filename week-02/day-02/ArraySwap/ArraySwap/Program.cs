446e2ddc6cc4f61740414ce8bdc4cbb84ed9f4d0
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 15:17:26 +0100

    feat(week-02/day-02):array swap

---
+++ week-02/day-02/ArraySwap/ArraySwap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArraySwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            string[] abc = { "first", "second", "third" };
+            string swapper = abc[0];
+            abc[0] = abc[2];
+            abc[2] = swapper;
+            Console.WriteLine(abc[0] + abc[2]);
+            Console.ReadKey();
+        }
+    }
+}
