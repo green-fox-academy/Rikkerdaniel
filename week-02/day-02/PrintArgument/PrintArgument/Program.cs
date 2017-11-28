e47e34040d39d737bb6a1417082ec2194fae7b37
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 13:47:27 +0100

    feat(week-02/day-02):Printer

---
+++ week-02/day-02/PrintArgument/PrintArgument/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintArgument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ar = { "first","second"};
+            Printer(ar );
+            Console.ReadKey();
+        }
+        static void Printer(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
