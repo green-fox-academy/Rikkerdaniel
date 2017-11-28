3cb0e9a111ff39b1df45674a22cfa836badba656
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 15:20:31 +0100

    feat(week-02/day-02):array sum

---
+++ week-02/day-02/ArraySum/ArraySum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = new int[] { 1, 2, 3 };
+            int sum = 0;
+            for (int i = 0; i < ai.Length; i++)
+            {
+                sum += ai[i];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
