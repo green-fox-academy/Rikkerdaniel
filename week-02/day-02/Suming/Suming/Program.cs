c879631b1a1956effe1de18907a1097c9dc95434
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 12:03:38 +0100

    feat(week-02/day-02):Sum method

---
+++ week-02/day-02/Suming/Suming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Suming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Give me a NUmber ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Sum(number);
+            Console.ReadKey();
+        }
+        static void Sum(int num)
+        {
+           int a= num * (num + 1) / 2;
+            Console.WriteLine(a );
+        }
+    }
+}
