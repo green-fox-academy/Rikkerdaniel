658e93a43ee4114126cc533f9d382397d4681fce
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 15:09:59 +0100

    feat(week-02/day-02):animals

---
+++ week-02/day-02/AppendA2/AppendA2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppendA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimals = { "kuty", "macsk", "cic" };
+            for (int i = 0; i < nimals.Length ; i++)
+            {
+                nimals[i] = nimals[i] + "a";
+            }
+            Console.WriteLine(nimals[1]);
+            Console.ReadKey();
+        }
+    }
+}
