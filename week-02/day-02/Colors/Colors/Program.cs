efb2798e10056ca9a17f0129b8db8a4c38e98caf
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 15:05:43 +0100

    feat(week-02/day-02):Colors

---
+++ week-02/day-02/Colors/Colors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] colors = new string[3][];
+            colors[0] = new string[] { "lime", "forest green", "olive", "pale green", "spring green" };
+            colors[1] = new string[] { "orange red", "red", "tomato" };
+            colors[2] = new string[] { "orchid", "violet", "pink", "hot pink" };
+        }
+    }
+}
