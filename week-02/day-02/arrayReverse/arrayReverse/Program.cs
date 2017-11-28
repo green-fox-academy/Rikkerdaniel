3dfb6bf31f4c1f0c151157b33cad08341bf74099
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 15:37:13 +0100

    feat(week-02/day-02):Reverse array

---
+++ week-02/day-02/arrayReverse/arrayReverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace arrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = { 3, 4, 5, 6, 7 };
+            Array.Reverse(aj);
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.WriteLine(aj[i] + ",");
+            }
+            Console.ReadKey();
+        }
+    }
+}
