372cd1087d0cc2a7d6ec3476118d5a92d6bf8192
Rikkerdaniel <rik.danka@gmail.com>
2017-11-28 12:20:16 +0100

    feat(week-02/day-02):Factorials

---
+++ week-02/day-02/Factorial/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me that numer :D");
+            int num = Convert.ToInt32(Console.ReadLine());
+            factorio(num);
+            Console.ReadKey();
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+        }
+        static void factorio(int number)
+        {
+            int a = number ;
+            for (int i = 1; i < number ; i++)
+            {
+                a = a * i;
+            }
+            Console.WriteLine("factorial of your number is : "+a );
+        }
+    }
+}
