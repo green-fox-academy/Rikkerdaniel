51f456d96a6e883cddb79777e4e5032cf77514f6
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 14:39:39 +0100

    feat(week-02/day-03):Planet list

---
+++ week-02/day-03/PLanetList/PLanetList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PLanetList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+            planetList.Insert(5, "Saturn,");
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- week-02/day-03/ReverseString/ReverseString/Program.cs
+++ week-02/day-03/ReverseString/ReverseString/Program.cs
@@ -7,16 +7,16 @@ namespace ReverseString
         static void Main(string[] args)
         {
             string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-            Console.WriteLine(   RThing(reversed));
+            Console.WriteLine(RThing(reversed));
             Console.ReadKey();
         }
         static string RThing(string a)
         {
             string b = "";
-            for (int i = a.Length-1; i >= 0; i--)
+            for (int i = a.Length - 1; i >= 0; i--)
             {
-                b = b+(Convert.ToString(a[i]));
-               
+                b = b + (Convert.ToString(a[i]));
+
             }
             return b;
 
