e4e41f0fbb155b311104c075448f79abaea9d4ac
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 12:36:18 +0100

    feat(week-02/day-03):UrlFixer

---
+++ week-02/day-03/UrlFix/UrlFix/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UrlFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            url = url.Replace("bot", "odd");
+
+            int last = url.IndexOf("/");
+
+            url = url.Insert(5, ":");
+            Console.WriteLine(url);
+            Console.ReadKey();
+        }
+    }
+}
