78db5ffdd6b5e32d9b0ad1211266c2086c98cd02
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 17:27:19 +0100

    feat(week-02/day-03):Total useless hello world form

---
+++ week-02/day-03/Calculator/Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can use +,-,*,/,%, examle : + 32 27");
+            Console.WriteLine("Give me what you what to calculate ");
+            char a = Convert.ToChar (Console.Read ());
+            Console.ReadKey( );
+            int b = Convert.ToInt32(Console.Read());
+            Console.ReadKey( );
+            int c = Convert.ToInt32(Console.Read());
+            Console.ReadKey();
+            Console.WriteLine(b + a+c );
+
+
+            //string v = Console.ReadLine();
+            //a.Split( );
+            //int b = Convert.ToInt32(a);
+            //Console.WriteLine(b );
+            //Console.WriteLine(Convert.ToInt32 ((a[1])) + (a[0])+Convert.ToInt32 (( a[2])));
+            Console.ReadKey();
+
+            // Create a simple calculator application which reads the parameters from the prompt
+            // and prints the result to the prompt.
+            // It should support the following operations:
+            // +, -, *, /, % and it should support two operands.
+            // The format of the expressions must be: {operation} {operand} {operand}.
+            // Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+            // You can use the Scanner class
+            // It should work like this:
+
+            // Start the program
+            // It prints: "Please type in the expression:"
+            // Waits for the user input
+            // Print the result to the prompt
+            // Exit
+        }
+    }
+}
