cee759686aa71bfd2e97cfbf4812c98e3745e73a
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 15:36:56 +0100

    feat(week-02/day-03):candyshop not correct

---
+++ week-02/day-03/Candyshop/Candyshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Candyshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+            
+            list.RemoveAt(1);
+             list.Insert(1, "Croissant");
+            list.Remove(false);
+            list.Add("Ice cream");
+            Console.WriteLine(list);
+            Console.ReadKey();
+        }
+    }
+}
