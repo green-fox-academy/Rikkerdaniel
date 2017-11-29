a0ab991c1d958fcb5259db01e788a00825a302a3
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 15:40:32 +0100

    feat(week-02/day-03):Contains

---
+++ week-02/day-03/Contains/Contains/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+            if (List.Contains(7))
+            {
+                Console.WriteLine("hoooraaaayyyyy");
+            }
+            else
+            {
+                Console.WriteLine("nooooo:(((");
+            }
+            Console.ReadKey();
+        }
+    }
+}
