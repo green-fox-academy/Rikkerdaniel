6e12ce93307b107b24f75538eccaa008119e2002
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 15:12:28 +0100

    feat(week-02/day-03):Append

---
+++ week-02/day-03/AppendLeter/AppendLeter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendLeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rok", "halacsk" };
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] += "a";
+                Console.WriteLine(far[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
