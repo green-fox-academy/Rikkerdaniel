5b891a00b4165dc797b9c6c4800263b24160035e
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 13:30:38 +0100

    feat(week-02/day-03):UrlTakes longer

--- week-02/day-03/TakesLonger/TakesLonger/Program.cs
+++ week-02/day-03/TakesLonger/TakesLonger/Program.cs
@@ -6,22 +6,12 @@ namespace TakesLonger
     {
         static void Main(string[] args)
         {
-            string url = "https//www.reddit.com/r/nevertellmethebots";
-            url = url.Replace("bot", "odd");
-
-            int last = url.IndexOf("/");
-
-            url = url.Insert(5, ":");
-            Console.WriteLine(url);
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+            int a  = quote.IndexOf("I");
+            a = a+2;
+            quote = quote.Insert(a, " always takes longer than");
+            Console.WriteLine(quote);
             Console.ReadKey();
-            //string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
-            //int  = url.IndexOf("/");
-
-            // When saving this quote a disk error has occured. Please fix it.
-            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
-            // Using pieces of the quote variable (instead of just redefining the string)
-
-            //Console.WriteLine(quote);
         }
     }
 }
