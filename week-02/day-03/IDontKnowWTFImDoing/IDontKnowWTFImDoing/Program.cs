ced536d8c88ef1b223eaa0bf2d05f04f71e745b1
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 16:03:46 +0100

    feat(week-02/day-03):Swap elements

---
+++ week-02/day-03/IDontKnowWTFImDoing/IDontKnowWTFImDoing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IDontKnowWTFImDoing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            var list2 = new List<string>();
+            list2.Add(list[2]);
+            list[2] = list[5];
+            list[5] = list2[0];
+            foreach (string item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
