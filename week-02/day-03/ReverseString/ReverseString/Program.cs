64c4705b6a8215886f363f4b4a1dcd84798f49ab
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 14:17:15 +0100

    feat(week-02/day-03):Reverse String

---
+++ week-02/day-03/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            Console.WriteLine(   RThing(reversed));
+            Console.ReadKey();
+        }
+        static string RThing(string a)
+        {
+            string b = "";
+            for (int i = a.Length-1; i >= 0; i--)
+            {
+                b = b+(Convert.ToString(a[i]));
+               
+            }
+            return b;
+
+        }
+    }
+}
