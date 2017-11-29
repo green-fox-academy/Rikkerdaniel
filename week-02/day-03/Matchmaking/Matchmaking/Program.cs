eaa1308525ee602fdbecf52e60373ce0edd88394
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 15:06:34 +0100

    feat(week-02/day-03):MatchMaking

---
+++ week-02/day-03/Matchmaking/Matchmaking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matchmaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string> { girls[0], boys[0] ,girls [1],boys [1] , girls[2], boys[2], girls[3], boys[3], girls[4], boys[4],  boys[5] };
+            Console.WriteLine(order);
+        }
+    }
+}
