b7d3c58067b8074634bc8ba41908438d0f0a3f06
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 17:33:26 +0100

    feat(week-02/day-03):Total useless hello world form  seccond button

---
+++ week-02/day-03/TRyHard/TRyHard/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TRyHard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+                MessageBox.Show("Hello world");
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" i said it is a useless buton why did you clicked on it ?");
+        }
+    }
+}
