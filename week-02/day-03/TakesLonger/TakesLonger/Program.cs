1baa695dd80f33293c3a261f6df40895ca128f82
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 13:26:13 +0100

    feat(week-02/day-03):UrlFixer done

---
+++ week-02/day-03/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            url = url.Replace("bot", "odd");
+
+            int last = url.IndexOf("/");
+
+            url = url.Insert(5, ":");
+            Console.WriteLine(url);
+            Console.ReadKey();
+            //string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+            //int  = url.IndexOf("/");
+
+            // When saving this quote a disk error has occured. Please fix it.
+            // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+            // Using pieces of the quote variable (instead of just redefining the string)
+
+            //Console.WriteLine(quote);
+        }
+    }
+}
--- week-02/day-03/UrlFix/UrlFix/Program.cs
+++ week-02/day-03/UrlFix/UrlFix/Program.cs
@@ -8,9 +8,6 @@ namespace UrlFix
         {
             string url = "https//www.reddit.com/r/nevertellmethebots";
             url = url.Replace("bot", "odd");
-
-            int last = url.IndexOf("/");
-
             url = url.Insert(5, ":");
             Console.WriteLine(url);
             Console.ReadKey();
