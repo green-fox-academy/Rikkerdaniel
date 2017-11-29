62e0f5120811b4eae57c890d0ac315ab7aa5e22a
Rikkerdaniel <rik.danka@gmail.com>
2017-11-29 13:40:44 +0100

    feat(week-02/day-03):Todo text

---
+++ week-02/day-03/TodoText/TodoText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TodoText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            todoText = todoText.Insert(0, "My todo:\n");
+            todoText = todoText.Insert(todoText.LastIndexOf("\n"), "\n - Download games");
+            todoText = todoText.Insert(todoText.LastIndexOf("\n"), "\n   - Diablo");
+            Console.WriteLine(todoText);
+            Console.ReadKey();
+        }
+    }
+}
