b25413ffa4be852b197c59dd2cd4208cd44edfd6
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 10:38:16 +0100

    feat(week-02/day-04):Sum and average

---
+++ week-02/day-04/SumAverage/SumAverage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me how many  numbers do you want to sum and average");
+            int counter = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            for (int i = 0; i < counter ; i++)
+            {
+                Console.WriteLine("Give me Another number");
+                a = a + Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("sum = "+ a +"" + "\n average = "+(a/counter ));
+            Console.ReadKey();
+            
+        }
+    }
+}
