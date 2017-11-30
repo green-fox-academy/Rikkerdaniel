220e52d0e8fbfb1f8a857d112019733381d791ce
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 11:15:59 +0100

    feat(week-02/day-04):chess table

---
+++ week-02/day-04/ChessTable/ChessTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChessTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("%  ");
+                }
+                Console.WriteLine("\n");
+                if (i%2==0)
+                {
+                    Console.Write(" ");
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
