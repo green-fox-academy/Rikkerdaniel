b1832f116965dcd05ad65769c430dfd8b5662fe9
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 12:35:23 +0100

    feat(week-02/day-04):sort list

---
+++ week-02/day-04/LIstSort/LIstSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LIstSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 34, 12, 24, 9, 5 };
+            list.Sort();
+            for (int i = 0; i < list.Count ; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
