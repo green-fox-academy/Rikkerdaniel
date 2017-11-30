c1c1c173039227d9573848635e7e7e7da8fd9311
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 08:28:05 +0100

    feat(week-02/day-03):fibonacci

---
+++ week-02/day-03/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+
+        static int fib(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wich fibonacci number do you want ??");
+
+
+            Console.WriteLine(fib(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+        }
+    }
+}
