0c57e3622de975cc6c37d570227dd5986bf9b609
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 12:27:23 +0100

    feat(week-02/day-04):uniqe list

---
+++ week-02/day-04/Uniq/Uniq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uniq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 11, 34, 11, 52, 61, 1, 34 };
+            var list2 = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!list2.Contains(list[i]))
+                {
+                    list2.Add(list[i]);
+                }
+            }
+            for (int i = 0; i < list2.Count; i++)
+            {
+                Console.Write(list2[i]+"  ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
