f18feeb540e7d582e18332f2f8852a5a0e30b8ef
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 11:50:54 +0100

    feat(week-02/day-04):Wawe hello

---
+++ week-02/day-04/Wawe/Wawe/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Wawe
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 100; i++)
+            {
+                 
+
+                Console.WriteLine("   *   ****");
+                Console.WriteLine("  * *  *  *");
+                Console.WriteLine("  * *  *  *");
+                Console.WriteLine("   *    **");
+                Console.WriteLine("   *   ****");
+                Console.WriteLine("   ********");
+                Console.WriteLine("       ****");
+                Console.WriteLine("       ****");
+                Console.WriteLine("       ****");
+                Console.WriteLine("       ****");
+                Console.WriteLine("     HELLO");
+                Thread.Sleep(600);
+                Console.Clear();
+                Console.WriteLine("  *    ****");
+                Console.WriteLine(" * *   *  *");
+                Console.WriteLine(" * *   *  *");
+                Console.WriteLine("  *     **");
+                Console.WriteLine("  *    ****");
+                Console.WriteLine("   ********");
+                Console.WriteLine("       ****");
+                Console.WriteLine("       ****");
+                Console.WriteLine("       ****");
+                Console.WriteLine("       ****");
+                Console.WriteLine("     HELLO");
+                Thread.Sleep(600);
+                Console.Clear();
+            }
+        }
+    }
+}
