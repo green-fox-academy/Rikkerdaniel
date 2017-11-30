645dfb1ea3a779212fa80b4ef69ec9737ec762af
Rikkerdaniel <rik.danka@gmail.com>
2017-11-30 11:05:09 +0100

    feat(week-02/day-04):Super awsome number guessing  game

---
+++ week-02/day-04/GuesTHeNumber/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int guessthis = r.Next(0, 10);
+            Console.WriteLine("I think a number between 0 and 10 , can you guess ?");
+            int end = 0;
+            while (end == 0)
+            {
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess > guessthis)
+                {
+                    Console.WriteLine("too big");
+
+                }
+                else if (guess < guessthis)
+                {
+                    Console.WriteLine("Too small");
+                }
+                else if (guess == guessthis)
+                {
+                    Console.WriteLine("Congrats you win");
+                    end++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
