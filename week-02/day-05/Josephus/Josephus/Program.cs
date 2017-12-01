f365af93c9cd89217b9aca8cceadf4b378e2605c
Rikkerdaniel <rik.danka@gmail.com>
2017-12-01 12:58:23 +0100

    feat(week-02/day-05)josephus skeleton

---
+++ week-02/day-05/Josephus/Josephus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Josephus
+{
+    class Program
+    {
+        static int Josephus(int n)
+        {
+            int step = 2;
+            if (n <= 2)
+            {
+                return 1;
+            }
+            int a= ((n - 1) + (step - 1) % (n + 1));
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dont want to die in a suicide circel ? Give me the number of people and i save your ass!");
+
+
+            Console.WriteLine(Josephus(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+        }
+    }
+}
