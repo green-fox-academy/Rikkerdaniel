b8294d1ddb6c1e9ad767314ccb0b7c6cfe3de7d8
Rikkerdaniel <rik.danka@gmail.com>
2017-12-01 11:37:43 +0100

    feat(week-02/day-05)armstrong number base

---
+++ week-02/day-05/ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me that number");
+            string  isarmstrong = Console.ReadLine();
+            char[] digitarray= isarmstrong.ToCharArray();
+            double  a = 0;
+            for (int i = 0; i < isarmstrong.Length ; i++)
+            {
+                string s = Convert.ToString(digitarray[i]);
+                double b = Convert.ToDouble(s );
+                a = a +( Math.Pow(b, isarmstrong.Length));
+            }
+            if (a==Convert.ToDouble (isarmstrong ))
+            {
+                Console.WriteLine("The "+ isarmstrong + " is an armstrong number :)");
+            }
+            else
+            {
+                Console.WriteLine("Sorry"+isarmstrong +" is not an armstrong number");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
