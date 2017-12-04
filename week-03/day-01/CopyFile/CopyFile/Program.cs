70e1ab8fe7ec03238ccc2d8646a5fb769756f679
Rikkerdaniel <rik.danka@gmail.com>
2017-12-04 14:39:19 +0100

    feat(week-03/day-01):copy file

---
+++ week-03/day-01/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Copy(string inp, string outp)
+        {
+            int a = 0;
+            try
+            {
+                File.Copy(inp, outp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("false");
+                a++;
+            }
+            if (a == 0)
+            {
+                Console.WriteLine("true");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the path of the file what you want to copy");
+            string path = Console.ReadLine();
+            Console.WriteLine("path of where you want to copy it");
+            string a = Console.ReadLine();
+            Copy(path, a);
+            Console.ReadKey();
+        }
+    }
+}
