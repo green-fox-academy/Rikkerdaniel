eb60e40443ac517317174411ae24735a42b7b830
Rikkerdaniel <rik.danka@gmail.com>
2017-12-04 11:58:29 +0100

    feat(week-03/day-01):read text

---
+++ week-03/day-01/PrintLines/PrintLines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace PrintLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string[] content = File.ReadAllLines(@".\my-file.txt");
+
+                for (int i = 0; i < content.Length; i++)
+                {
+                    Console.WriteLine(content[i]);
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Uh -oh, could not read the file!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
