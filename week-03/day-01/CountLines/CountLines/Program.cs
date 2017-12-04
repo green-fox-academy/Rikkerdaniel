f7294def7d04a7e2db147ba41e28436ed0b8e28d
Rikkerdaniel <rik.danka@gmail.com>
2017-12-04 12:10:37 +0100

    feat(week-03/day-01):count lines

---
+++ week-03/day-01/CountLines/CountLines/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me the filename");
+            string st = Console.ReadLine();
+            try
+            {
+                string[] b = File.ReadAllLines(st );
+                int a = b.Length;
+                Console.WriteLine(a );
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("0");
+            }
+            Console.ReadKey();
+           
+            
+        }
+    }
+}
