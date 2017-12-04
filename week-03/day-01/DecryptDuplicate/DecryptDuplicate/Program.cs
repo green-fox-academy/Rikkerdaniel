f150b32e8cc86b68f65c7b31dfd9a5b69efce17f
Rikkerdaniel <rik.danka@gmail.com>
2017-12-04 15:44:52 +0100

    feat(week-03/day-01):Remove duplicate chars

---
+++ week-03/day-01/DecryptDuplicate/DecryptDuplicate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecryptDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string str = "";
+            var file = new StreamReader(@"C:\Users\rikda\Desktop\asbest\greenfox\Rikkerdaniel\week-03\day-01\DecryptDuplicate\DecryptDuplicate\DuplicatedChars.txt");
+
+            String line = file.ReadToEnd();
+            char[] chars = line.ToCharArray();
+            for (int i = 0; i < chars.Length ;i++)
+            {
+                if (i%2==1)
+                {
+                    str += chars[i].ToString();
+                }
+                
+
+            }
+            file.Close();
+            Console.WriteLine(str );
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
