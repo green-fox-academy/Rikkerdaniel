d7a6203361c301dc8b37c2f0eb41f6e5de8f0bfe
Rikkerdaniel <rik.danka@gmail.com>
2017-12-04 14:07:03 +0100

    feat(week-03/day-01):Write one line

---
+++ week-03/day-01/WriteLine/WriteLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter file2 = new StreamWriter(@"c:\my-file.txt");
+                file2.WriteLine("dani vagyook hello");
+                file2.Close();
+            }
+            catch(Exception )
+            {
+                Console.WriteLine("This is a shit file not found");
+            }
+            Console.ReadKey();
+        }
+    }
+}
