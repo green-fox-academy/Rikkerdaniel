3c0d3bb44dff0ebce25ad1a5e3cb7ef363a84c1a
Rikkerdaniel <rik.danka@gmail.com>
2017-12-06 17:27:12 +0100

    feat(week-03/day-02):tryangles not good solution

---
+++ week-03/day-02/tryangle/tryangle/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace tryangle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawGreenLines(foxDraw);
+        }
+        public static void DrawGreenLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 350, 350, 350);
+            foxDraw.DrawLine(175, 50, 350, 350);
+            foxDraw.DrawLine(175, 50, 0, 350);
+            for (int i = 0; i < 17; i++)
+            {
+                foxDraw.DrawLine(10+(i*10), 340-(i*18), 10+(i*20),350);
+                foxDraw.DrawLine(350 - (i * 10), 340- (i * 18), 350- (i * 20), 350);
+            }
+
+
+        }
+    }
+}
