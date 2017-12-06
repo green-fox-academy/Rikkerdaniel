4faeb7cd2c1b295c3cea03e4e0e96efa2c6661ad
Rikkerdaniel <rik.danka@gmail.com>
2017-12-06 11:21:57 +0100

    feat(week-03/day-02):go to center

---
+++ week-03/day-02/GotoCenter/GotoCenter/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace GotoCenter
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawGreenLines(foxDraw);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // draw 3 lines with that function.
+        }
+        public static void DrawGreenLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+
+            for (int i = 2; i < 5; i++)
+            {
+                foxDraw.DrawLine((10+(i+43)),(20+(i*15)), 175, 175);
+            }
+           
+        }
+    }
+}
