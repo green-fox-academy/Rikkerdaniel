59e17c6c6abf44c3c965fa76c575702e7ba94f20
Rikkerdaniel <rik.danka@gmail.com>
2017-12-06 15:07:35 +0100

    feat(week-03/day-02):many lines to center

---
+++ week-03/day-02/manyLines/manyLines/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace manyLines
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawLines(foxDraw);
+        }
+        public static void DrawLines(FoxDraw foxDraw)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                foxDraw.StrokeColor(Colors.Green);
+                foxDraw.DrawLine(0 + (i * 20), 0, 100, 100);
+                foxDraw.DrawLine(200, 0 + (i * 20), 100, 100);
+                foxDraw.DrawLine(0, 0 + (i * 20), 100, 100);
+                foxDraw.DrawLine(0 + (i * 20), 200, 100, 100);
+
+            }
+        }
+    }
+}
