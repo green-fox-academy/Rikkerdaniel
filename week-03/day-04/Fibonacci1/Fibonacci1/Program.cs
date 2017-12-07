5f179c1708ae04b7a883d2efca67138a754143e6
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 14:04:06 +0100

    feat(week-03/day-04):Fibonacci base

---
+++ week-03/day-04/Fibonacci1/Fibonacci1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci1
+{
+    class Program
+    {
+        static int Fib(int n)
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wich fibonacci number do you want ??");
+
+
+            Console.WriteLine(Fib(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+        }
+    }
+}
