793d26187d7b66af8543036a64dfc737bf2c3615
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 13:55:53 +0100

    feat(week-03/day-04):string x to y correction

--- week-03/day-04/String/String/Program.cs
+++ week-03/day-04/String/String/Program.cs
@@ -16,17 +16,9 @@ namespace String
         }
         static string Replace(string text)
         {
-            if (text.Length == 0)
-            {
-                return "";
-            }
-            if (text[0] == 'x')
-            {
-                text = "y" + text.Substring(1);
-            }
-            
-              return text[0] + Replace(text.Substring(1));
+            text = text.Replace("x", "y");
+            return text;
         }
-       
+
     }
 }
