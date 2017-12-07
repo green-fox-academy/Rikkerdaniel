9f4c6735c05f9506750c1251f75fc0e7250dc2c6
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 10:56:22 +0100

    feat(week-03/day-04):Add numbers

---
+++ week-03/day-04/AddNumber/AddNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            AddNumber(inputNum, 0);
+            Console.ReadLine();
+        }
+        static int AddNumber(int n, int count)
+        {
+            if (count <= n)
+            {
+                Console.WriteLine(count);
+                return AddNumber(n, count + 1);
+            }
+            else
+            {
+
+                return count;
+            }
+        }
+
+    }
+}
