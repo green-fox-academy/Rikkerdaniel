ff9896dbb8b6300713aaf50dc5753aa2237b2a22
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 11:55:28 +0100

    feat(week-03/day-04):bunnys

---
+++ week-03/day-04/Bunnys/Bunnys/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunnys = int.Parse(Console.ReadLine());
+            int ear = 0;
+            Console.WriteLine( EarCounter(bunnys, ear));
+            Console.ReadLine();
+        }
+        static int EarCounter(int bunny,int ears)
+        {
+            if (bunny >0)
+            {
+                return EarCounter(bunny - 1, ears + 2);
+            }
+            else
+            {
+                return ears;
+            }
+        }
+    }
+}
