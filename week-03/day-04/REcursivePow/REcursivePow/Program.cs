3828cfc35b756fa4000872a18b0942fcb4f25a3a
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 11:47:42 +0100

    feat(week-03/day-04):Pow recursive

---
+++ week-03/day-04/REcursivePow/REcursivePow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REcursivePow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given base and n that are both 1 or more, compute recursively (no loops)
+            // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+            int bas = 4;
+            int pow = 3;
+            int count = 1;
+            Console.WriteLine(   PowStupiditi(bas, pow,count));
+            Console.ReadKey();
+        }
+        static int PowStupiditi(int bas1,int n,int counter)
+        {
+            if (n  >0)
+            {
+               return  PowStupiditi(bas1, n-1 ,counter *bas1);
+            }
+            else
+            {
+                return counter;
+            }
+        }
+    }
+}
