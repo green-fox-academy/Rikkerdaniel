706f5842f140c3855d3c5be771ac26eec83de361
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 11:26:34 +0100

    feat(week-03/day-04):Sum thing

---
+++ week-03/day-04/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse (Console.ReadLine());  
+            Console.WriteLine(SumDigit(number));
+            Console.ReadLine();
+        }
+        private static int SumDigit(int number)
+        {
+            if (number / 10 > 0)
+            {
+                Console.WriteLine(number);
+                return number % 10 + SumDigit(number / 10);
+            }
+            else
+            {
+                return number % 10;
+            }
+        }
+    }
+}
+
