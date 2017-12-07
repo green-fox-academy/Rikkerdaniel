f9180a0134c3ac13677a9725710ff091526a805a
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 12:06:21 +0100

    feat(week-03/day-04):mutant bunnys

---
+++ week-03/day-04/MoreBunnys/MoreBunnys/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreBunnys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunnys = int.Parse(Console.ReadLine());
+            int ears = 0;
+            Console.WriteLine(WierdBunnys(bunnys, ears));
+            Console.ReadKey();
+        }
+        static int WierdBunnys(int bunny, int ear)
+        {
+            if (bunny % 2 == 0 && bunny != 0)
+            {
+                return WierdBunnys(bunny - 1, ear + 3);
+            }
+            else if (bunny % 2 == 1)
+            {
+                return WierdBunnys(bunny - 1, ear + 2);
+            }
+            else
+            {
+                return ear;
+            }
+        }
+    }
+}
