e67fb08cd40d102eda91ae14cf65c53713461e95
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 10:38:47 +0100

    feat(week-03/day-04):Count down recursive

---
+++ week-03/day-04/CountDown/CountDown/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            CountDown(a);
+            Console.ReadKey();
+        }
+        static int CountDown(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(n);
+                return CountDown(n - 1);
+            }
+            else
+            {
+                return n;
+            }
+
+        }
+    }
+}
