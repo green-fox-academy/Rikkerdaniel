9faa1baf2da7bdc774f1c765b51f65d88c1352c9
Rikkerdaniel <rik.danka@gmail.com>
2017-12-07 13:51:49 +0100

    feat(week-03/day-04):string2 x to none

---
+++ week-03/day-04/String2/String2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string thing = Console.ReadLine();
+            Console.WriteLine(Replace(thing));
+            Console.ReadKey();
+        }
+        static string Replace(string text)
+        {
+            text=text.Replace("x",string.Empty );
+            return text;
+        }
+    }
+}
