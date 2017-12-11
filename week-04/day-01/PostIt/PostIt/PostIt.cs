c0ee1f40a6e901e8d28e55d7e2c1f8d3e2ed6825
Rikkerdaniel <rik.danka@gmail.com>
2017-12-11 11:55:33 +0100

    feat(Week-04/day-01):postit thing

---
+++ week-04/day-01/PostIt/PostIt/PostIt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    public struct PostIt
+    {
+        public string BackgrounColor;
+        public string Text;
+        public string TextColor;
+    }
+}
---
+++ week-04/day-01/PostIt/PostIt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var postit = new PostIt();
+            var postit1 = new PostIt();
+            var postit2 = new PostIt();
+            postit.BackgrounColor = "orange";
+            postit.Text = "idea1";
+            postit.TextColor = "Blue";
+
+            postit1.BackgrounColor = "pink";
+            postit1.Text = "Awsome";
+            postit1.TextColor = "Black";
+
+            postit2.BackgrounColor = "yellow";
+            postit2.Text = "super";
+            postit2.TextColor = "green";
+
+        }
+    }
+}
