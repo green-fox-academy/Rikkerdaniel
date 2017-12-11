48c62ff78d6b072e7482a2ff2b635528bed3180a
Rikkerdaniel <rik.danka@gmail.com>
2017-12-11 17:19:38 +0100

    feat(Week-04/day-01):Sharpie

---
+++ week-04/day-01/Sharpie/Sharpie/SharpieC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class SharpieC
+    {
+        public SharpieC(string color, float width)
+        {
+            color = color;
+            width = width;
+            inkAmount = 100;
+        }
+
+        public string color { get; set; }
+        public float width { get; set; }
+        public float inkAmount { get; set; }
+
+        public void Use()
+        {
+            inkAmount--;
+        }
+    }
+}
