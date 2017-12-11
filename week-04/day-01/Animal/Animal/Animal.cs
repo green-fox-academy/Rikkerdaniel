712d832a5dfbe2add0193252962f67ccf20a418d
Rikkerdaniel <rik.danka@gmail.com>
2017-12-11 17:08:28 +0100

    feat(Week-04/day-01):animals

---
+++ week-04/day-01/Animal/Animal/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public Animal()
+        {
+            hunger = 50;
+            thirst = 50;
+        }
+        public int hunger { get; set; }
+        public int thirst { get; set; }
+
+        public void Eat()
+        {
+            hunger--;
+        }
+
+        public void Drink()
+        {
+            thirst--;
+        }
+
+        public void Play()
+        {
+            hunger++;
+            thirst++;
+        }
+    }
+}
