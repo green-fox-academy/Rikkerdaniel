c430a586253f4170b471acf6d7e96707873146d7
Rikkerdaniel <rik.danka@gmail.com>
2017-12-12 12:18:21 +0100

    feat(week-04/day-02):Greenfox student class

--- week-04/day-02/GreenfoxOO/GreenfoxOO/Person.cs
+++ week-04/day-02/GreenfoxOO/GreenfoxOO/Person.cs
@@ -8,5 +8,35 @@ namespace GreenfoxOO
 {
     class Person
     {
+        public Person(string name ,int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public Person()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "Female";
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string  Gender { get; set; }
+
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm "+Name+", a "+Age+" year old "+Gender);
+        }
+
+        public virtual void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+        
     }
+
 }
---
+++ week-04/day-02/GreenfoxOO/GreenfoxOO/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenfoxOO
+{
+    class Student : Person
+    {
+        public Student(string name, int age, string gender, string PreviousOrganization)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            previousOrganization = PreviousOrganization;
+            skippedDays = 0;
+        }
+        public Student()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "Female";
+            previousOrganization = "School of life";
+            skippedDays = 0;
+
+        }
+        
+        public string previousOrganization { get; set; }
+        public int skippedDays { get; set; }
+        public override void GetGoal()
+        {
+            Console.WriteLine("Be a junior software developer.");
+        }
+
+        public new void Introduce()
+        {
+            Console.WriteLine("Hi, I'm " + Name + ", a " + Age + " year old" + Gender + " from " 
+                + previousOrganization + " who skipped "+skippedDays +"days from the course already.");
+        }
+
+        public int SkipDays(int numberOfDays)
+        {
+            skippedDays += numberOfDays;
+            return skippedDays;
+        }
+    }
+}
