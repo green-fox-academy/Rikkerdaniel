0e8f051a5e08b13f1763c6391658351af566deb8
Rikkerdaniel <rik.danka@gmail.com>
2017-12-12 13:26:41 +0100

    feat(week-04/day-02):Greenfox sponsor class

---
+++ week-04/day-02/GreenfoxOO/GreenfoxOO/Sponsor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenfoxOO
+{
+    class Sponsor:Person
+    {
+        public Sponsor(string name,int age , string gender,string Company)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            company = Company;
+        }
+        public Sponsor()
+        {
+            Name = "Jane Doe";
+            Age = 30;
+            Gender = "Female";
+            company = "Google";
+            hiredStudents = 0;
+        }
+
+        public string company { get; set; }
+        public int hiredStudents { get; set; }
+
+        public override void GetGoal()
+        {
+            Console.WriteLine( "Hire brilliant junior software developers");
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine("Hi, I'm name, a age year old gender who represents company and hired hiredStudents students so far.");
+        }
+        public int Hire()
+        {
+           hiredStudents = hiredStudents + 1;
+            return hiredStudents;
+        }
+    }
+}
