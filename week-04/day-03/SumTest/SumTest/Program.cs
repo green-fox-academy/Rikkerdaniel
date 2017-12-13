48aa7f5f47f9d678d01cc2273ea975f818a9d51f
Rikkerdaniel <rik.danka@gmail.com>
2017-12-13 12:12:57 +0100

    feat(week-04/day-03):Test Sum

---
+++ week-04/day-03/SumTest/SumTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+        public int Summer()
+        {
+            var intlist = new List<int>();
+            intlist.Add(0);
+            intlist.Add(1);
+            intlist.Add(2);
+
+            int sum = 0;
+            for (int i = 0; i < intlist.Count; i++)
+            {
+                sum += intlist[i];
+            }
+                return sum;
+        }
+    }
+}
---
+++ week-04/day-03/SumTest/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using SumTest;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //Arrange
+            var program = new Program();
+            //act
+           int result = program.Summer();
+            //assert
+            Assert.AreEqual(3,result);
+        }
+    }
+}
