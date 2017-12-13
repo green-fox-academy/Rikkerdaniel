55012ca2646491730027637b795fec316519b2b3
Rikkerdaniel <rik.danka@gmail.com>
2017-12-13 11:52:43 +0100

    feat(week-04/day-03):Test Apple

---
+++ week-04/day-03/TestApple/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9051980a-eb73-4be8-91ae-0b9918299ec6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ week-04/day-03/TestApple/Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using TestApple;
+
+
+namespace Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //arrange
+            var program = new Program();
+
+            //act
+            string a = program.Asd();
+
+
+            //assert
+            Assert.AreEqual("apple", a);
+        }
+    }
+}
