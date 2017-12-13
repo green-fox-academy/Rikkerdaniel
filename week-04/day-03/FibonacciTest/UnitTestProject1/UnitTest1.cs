a83127205de4770f491062132e318e8ec9a19cb3
Rikkerdaniel <rik.danka@gmail.com>
2017-12-13 14:46:18 +0100

    feat(week-04/day-03):Test Fibonacci

---
+++ week-04/day-03/FibonacciTest/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using FibonacciTest;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //Arrange
+            var prog = new Program();
+            //Act
+            int output = prog.Fib(5);
+            //Assert
+            Assert.AreEqual(5,output);
+        }
+    }
+}
