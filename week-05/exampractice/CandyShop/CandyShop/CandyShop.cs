4caba2eb3621100e9c86a56b25dd4ffa60129649
Rikkerdaniel <rik.danka@gmail.com>
2017-12-21 16:22:18 +0100

    fix(MySuperCandyShop): refactor

--- week-05/exampractice/CandyShop/CandyShop/CandyShop.cs
+++ week-05/exampractice/CandyShop/CandyShop/CandyShop.cs
@@ -97,8 +97,6 @@ namespace MySuperCandyShop
             foreach (Sweets sweet in CandyStorage)
             {
                 sweet.Price = sweet.Price * (1.0+Convert.ToDouble(percentage) / 100.0);
-                //SweetySweety.Price = SweetySweety.Price * (Convert.ToDouble(percentage) / 100.0);
-
             }
         }
 
