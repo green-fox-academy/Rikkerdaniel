10a1eeb11173860b1878d7dfc6c3ac76f93e7fb7
Rikkerdaniel <rik.danka@gmail.com>
2017-12-21 13:58:25 +0100

    feat(CandyShop):base and sweets candy and lollipop classes

---
+++ week-05/exampractice/CandyShop/CandyShop/Sweets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShop
+{
+    public class Sweets
+    {
+        public double Price { get; set; }
+        public double Sugar { get; set; }
+        public string Type { get; set; }
+
+        public Sweets(double price, double sugar, string type)
+        {
+            Price = price;
+            Sugar = sugar;
+            Type = type;
+        }
+    }
+}
