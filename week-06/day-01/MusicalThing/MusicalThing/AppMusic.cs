2018ea6bb314a8065964f904f0987650dbf48789
Rikkerdaniel <rik.danka@gmail.com>
2018-01-02 16:52:01 +0100

    feat(week-06/day1):music thing now plays music

--- week-06/day-01/MusicalThing/MusicalThing/AppMusic.cs
+++ week-06/day-01/MusicalThing/MusicalThing/AppMusic.cs
@@ -14,13 +14,15 @@ namespace Musical
 
             Console.WriteLine("Test 1 Play");
             guitar.Play();
+            Console.Beep(659, 325); Console.Beep(659, 325); Console.Beep(659, 325);
             bassGuitar.Play();
+            Console.Beep(584, 525);
             violin.Play();
+            Console.Beep(784, 125); Console.Beep(784, 125);
 
             Console.WriteLine("Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .");
             ElectricGuitar guitar2 = new ElectricGuitar(7);
             BassGuitar bassGuitar2 = new BassGuitar(5);
-
             Console.WriteLine("Test 2 Play");
             guitar2.Play();
             bassGuitar2.Play();
