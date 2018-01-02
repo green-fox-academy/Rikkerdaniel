dd05ba242618b833d7349235e782745e3bca78d2
Rikkerdaniel <rik.danka@gmail.com>
2018-01-02 17:22:11 +0100

    feat(week-06/day1):zoo base

---
+++ week-06/day-01/ZooThing/ZooThing/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooThing
+{
+    public abstract class Animal
+    {
+        protected string name;
+        protected int age;
+        protected string gender;
+        protected string color;
+        protected int NumberOfLegs;
+        public abstract void Greet();
+        public abstract void WantChild();
+    }
+}
---
+++ week-06/day-01/ZooThing/ZooThing/Reptile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooThing
+{
+   
+    
+        class Reptile : Animal
+     {
+         public Reptile(string name) : base(name)
+         {
+ 
+         }
+         public override void Greet()
+         {
+             Console.WriteLine("hello reptile");
+         }
+ 
+         public override void WantChild()
+         {
+            Console.WriteLine( "want a child from an egg");
+        }
+     }
+}
+
