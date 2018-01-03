99efe3a8696567fc634ac527fac1dc89a9471572
Rikkerdaniel <rik.danka@gmail.com>
2018-01-03 13:35:29 +0100

    feat(week-06/day2):ex2 fix nov works with negativ numbers to

--- week-06/day-02/LINQ/ex2/Program.cs
+++ week-06/day-02/LINQ/ex2/Program.cs
@@ -11,7 +11,9 @@ namespace ex2
         static void Main(string[] args)
         {
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
-            var averageOfOdds = n.Where(p => (p % 2) == 1).Average();
+            var averageOfOdds = n.Where(p => (p % 2) == 1||(p%2)== -1).Average(p=>p);
+            Console.WriteLine(averageOfOdds);
+            Console.ReadKey();
         }
     }
 }
