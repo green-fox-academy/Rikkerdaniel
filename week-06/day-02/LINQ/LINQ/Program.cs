e445373809a7d02053d52332faaef7033fcd007d
Rikkerdaniel <rik.danka@gmail.com>
2018-01-03 12:14:54 +0100

    feat(week-06/day2):linq ex 7

--- week-06/day-02/LINQ/LINQ/Program.cs
+++ week-06/day-02/LINQ/LINQ/Program.cs
@@ -13,10 +13,15 @@ namespace LINQ
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
             char[] c = { 'a', 'b', 'c', 'd' };
             var evens = n.Where(p => (p % 2) == 0);
+
             var averageOfOdds = n.Where(p => (p % 2) == 1).Average();
+
             var squaredOfPositive = n.Where(p => p > 0).Select(p => p * p);
+
             var squaredIsMoreThan20 = n.Where(p => (p * p) > 20);
+
             int[] n1 = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
             var valuesCounted = (from r in n1
                                  group r by r.CompareTo(r)
                                  into g
@@ -27,6 +32,9 @@ namespace LINQ
             }
 
             var charToString = c.ToString();
+
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            var StartAEdndI = cities.Where(p => p.StartsWith("A")).Where(p => p.EndsWith("I"));
             Console.ReadKey();
         }
     }
