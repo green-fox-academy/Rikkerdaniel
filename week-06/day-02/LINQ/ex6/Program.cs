b6f3577b95e8ed0569d07b257e1c5500b97ac5b3
Rikkerdaniel <rik.danka@gmail.com>
2018-01-03 13:24:48 +0100

    feat(week-06/day2):create ex 6

---
+++ week-06/day-02/LINQ/ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "ashgasvflksjvd aejosdbv svlciaeuovfdjbcn ";
+
+            var frequencyOfChars = from x in a
+                                  group x by x into y
+                                  select y;
+
+            foreach (var charNo in frequencyOfChars)
+            {
+                Console.WriteLine("Letter " + charNo.Key + " appears " + charNo.Count() + " times");
+            }
+            Console.ReadKey();
+        }
+    }
+}
