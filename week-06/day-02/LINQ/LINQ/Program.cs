47b3067c09a946ef3f5bc14555a9ac1bfd7faa6a
Rikkerdaniel <rik.danka@gmail.com>
2018-01-03 11:41:55 +0100

    feat(week-06/day2):linq ex 3

--- week-06/day-02/LINQ/LINQ/Program.cs
+++ week-06/day-02/LINQ/LINQ/Program.cs
@@ -13,6 +13,7 @@ namespace LINQ
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
             var evens = n.Where(p => (p % 2) == 0);
             var averageOfOdds = n.Where(p => (p % 2) == 1).Average();
+            var squaredOfPositive = n.Where(p => p > 0).Select(p => p * p);
         }
     }
 }
