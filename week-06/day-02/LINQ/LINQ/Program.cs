b5b415b64c5cf0c15d53f8a6ac545e7d48461f72
Rikkerdaniel <rik.danka@gmail.com>
2018-01-03 11:40:24 +0100

    feat(week-06/day2):linq ex 1

---
+++ week-06/day-02/LINQ/LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var evens = n.Where(p => (p % 2) == 0);
+        }
+    }
+}
