a452aab86557c1ce7f546a23b0df6dcc8cf6e435
Rikkerdaniel <rik.danka@gmail.com>
2018-01-08 14:47:29 +0100

    feat(week-07/day-01):greeting update )

---
+++ week-07/day-01/WebApplication1/WebApplication1/Controllers/RESTController1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api")]
+    public class RESTController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var greet = new Greeting(1,"Árvíztűrő tükörfúrógép");
+            return new JsonResult(greet);
+        }
+    }
+}
---
+++ week-07/day-01/WebApplication1/WebApplication1/Models/Greeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+
+        public Greeting(long id ,string content)
+        {
+            Id = id;
+            Content = content;
+        }
+    }
+}
