0ae74e8d5372aeb4b72a7b3dba8eabb12260a199
Rikkerdaniel <rik.danka@gmail.com>
2018-01-08 15:57:43 +0100

    feat(week-07/day-01):hello web)

---
+++ week-07/day-01/WebApplication1/WebApplication1/Controllers/WebController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("web")]
+    public class WebController : Controller
+    {
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var greeting = new Greeting(1,"World")
+            {
+            };
+
+            return View(greeting);
+        }
+    }
+}
