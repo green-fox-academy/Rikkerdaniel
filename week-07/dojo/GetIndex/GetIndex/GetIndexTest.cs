c53648c2e5345f0438e8e51dabcf0b7a6fb4acc8
Rikkerdaniel <rik.danka@gmail.com>
2018-01-09 10:56:33 +0100

    feat(dojo):super tests of get index app

---
+++ week-07/dojo/GetIndex/GetIndex/GetIndexTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GetIndex
+{
+    [TestFixture]
+    class GetIndexTest
+    {
+        Program prog = new Program();
+       
+        [TestCase]
+        public void ReturnMinus1()
+        {
+            Assert.AreEqual(-1, prog.GiveIndex(2316));
+        }
+
+        [TestCase]
+        public void ReturnTheIndexOfAnEvilNumber()
+        {
+            Assert.AreEqual(2, prog.GiveIndex(666));
+        }
+
+        [TestCase]
+        public void ReturnTheIndexOfAnotherNumber()
+        {
+            Assert.AreEqual(1, prog.GiveIndex(22));
+        }
+
+        [TestCase]
+        public void ReturnTheIndexOfATotallyDifferentNumber()
+        {
+            Assert.AreEqual(0, prog.GiveIndex(1));
+        }
+    }
+}
---
+++ week-07/dojo/GetIndex/GetIndex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int GiveIndex(int number)
+        {
+            List<int> SuperList = new List<int> { 1, 22, 666 };
+
+            try
+            {
+                var index = SuperList.IndexOf(number);
+                return index;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
