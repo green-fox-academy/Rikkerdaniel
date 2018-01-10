8f6b524d1e3a61336dd1a0eac6b7f17518f9cb61
Rikkerdaniel <rik.danka@gmail.com>
2018-01-10 15:03:05 +0100

    feat(week-07/day-03):simba bank third ex

--- week-07/day-03/SimbaBank/SimbaBank/Controllers/SimbaController.cs
+++ week-07/day-03/SimbaBank/SimbaBank/Controllers/SimbaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SimbaBank.Models;
+using SimbaBank.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,11 +13,57 @@ namespace SimbaBank.Controllers
     [Route("")]
     public class SimbaController : Controller
     {
+        public static List<BankAccount> Account  = new List<BankAccount>()
+        {
+        #region
+            new BankAccount()
+             {
+                Name = "Simba",
+                Balance = 2000.01F,
+                Currency ="Zebra",
+                AnimalType = "Animal.Lion"
+
+             },
+             new BankAccount()
+             {
+                Name = "aslan",
+                Balance = 21230.01F,
+                Currency ="Zebra",
+                AnimalType = "Animal.Lion"
+
+             },
+              new BankAccount()
+             {
+                Name = "Harambe",
+                Balance = 4320.01F,
+                Currency ="Banana",
+                AnimalType = "Animal.Gorilla"
+
+             },
+               new BankAccount()
+             {
+                Name = "Pumba",
+                Balance = 10000.01F,
+                Currency ="bug",
+                AnimalType = "Animal.Pig"
+
+             },
+                new BankAccount()
+             {
+                Name = "Timon",
+                Balance = 123400.01F,
+                Currency ="bug",
+                AnimalType = "Animal.Suricata"
+
+             }
+
+#endregion
+        };
+
         [Route("")]
         public IActionResult Index()
         {
-            var simba = new BankAccount() { Name = "Simba", Balance = 2000.01F,Currency="Zebra", AnimalType = "Animal.Lion" };
-            return View(simba);
+            return View(Account);
         }
     }
 }
---
+++ week-07/day-03/SimbaBank/SimbaBank/ViewModels/AccountViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SimbaBank.Models;
+
+namespace SimbaBank.ViewModels
+{
+    public class AccountViewModel
+    {
+        public List<BankAccount> AccountList  = new List<BankAccount>();
+
+        public void AddAccount(BankAccount bankAccount)
+        {
+            AccountList.Add(bankAccount);
+        }
+    }
+}
