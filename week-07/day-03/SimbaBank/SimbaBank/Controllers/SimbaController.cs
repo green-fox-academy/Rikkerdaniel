fc02a27d9ffd08fd64385041e1318be482abe04d
Rikkerdaniel <rik.danka@gmail.com>
2018-01-10 14:20:39 +0100

    feat(week-07/day-03):simba bank firsf and seccond ex

---
+++ week-07/day-03/SimbaBank/SimbaBank/Controllers/SimbaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimbaBank.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimbaBank.Controllers
+{
+    [Route("")]
+    public class SimbaController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            var simba = new BankAccount() { Name = "Simba", Balance = 2000.01F,Currency="Zebra", AnimalType = "Animal.Lion" };
+            return View(simba);
+        }
+    }
+}
---
+++ week-07/day-03/SimbaBank/SimbaBank/Models/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimbaBank.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public float Balance { get; set; }
+        public string Currency { get; set; }
+        public string  AnimalType { get; set; }
+    }
+}
