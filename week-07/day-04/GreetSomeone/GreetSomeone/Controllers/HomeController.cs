6763e904375ad0b7622444feaf72583f41c6db30
Rikkerdaniel <rik.danka@gmail.com>
2018-01-11 14:09:23 +0100

    feat(week-07/day-04):di ex 2

---
+++ week-07/day-04/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeone.Controllers
+{
+   
+
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public Name name;
+        public HomeController(Name name)
+        {
+            this.name = name;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(name);
+        }
+        [HttpPost("add")]
+        public IActionResult PostName(Name name)
+        {
+            this.name.UserName = name.UserName;
+            return RedirectToAction("GetName");
+        }
+
+        [HttpGet("greet")]
+        public IActionResult GetName()
+        {
+            return View(name);
+        }
+    }
+}
