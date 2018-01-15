7c68bb8535ac995ff1b23700f0f70e9bd260ef9f
Rikkerdaniel <rik.danka@gmail.com>
2018-01-15 12:48:31 +0100

    feat(week-08/day-01):todo webapp base database

---
+++ week-08/day-01/TodoFromDB/TodoFromDB/Entities/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoFromDB.Models;
+using JetBrains.Annotations;
+
+namespace TodoFromDB.Entities
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+              public DbSet<Todo> Todos { get; set; }
+    }
+}
+
+
