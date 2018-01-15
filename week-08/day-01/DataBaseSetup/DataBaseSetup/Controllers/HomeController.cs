f3b0968d0e99417d31001cbbbe32a19099de9e9a
Rikkerdaniel <rik.danka@gmail.com>
2018-01-15 12:22:32 +0100

    feat(week-08/day-01):first database setup

---
+++ week-08/day-01/DataBaseSetup/DataBaseSetup/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DataBaseSetup.Entities;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DataBaseSetup.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private TestContext testContext;
+ 
+         public HomeController(TestContext testContext)
+         {
+             this.testContext = testContext;
+         }
+
+    [HttpGet("")]
+        public IActionResult Index()
+        {
+            var SomefromDataBase = testContext.Tests.Where(p => p.Id == 1);
+            return Json(SomefromDataBase);
+        }
+    }
+}
---
+++ week-08/day-01/DataBaseSetup/DataBaseSetup/Migrations/20180115111206_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DataBaseSetup.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Number = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tests");
+        }
+    }
+}
