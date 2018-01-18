b5f5c47343633f15f3d443bcb3b14a4b19b5cc78
Rikkerdaniel <rik.danka@gmail.com>
2018-01-18 10:45:39 +0100

    feat(Reddit): able to add new post

---
+++ week-08/reddit/RedditProject/RedditProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedditProject.Repositories;
+
+namespace RedditProject.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+
+        public HomeController(RedditRepository redditRepository)
+        {
+            RedditRepository = redditRepository;
+        }
+
+        public RedditRepository RedditRepository { get; set; }
+
+        [Route("posts")]
+        public IActionResult Index()
+        {
+            return View(RedditRepository.PostList());
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost("addnew")]
+        public IActionResult AddNew(string content)
+        {
+           
+           RedditRepository.AddPost(content);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-08/reddit/RedditProject/RedditProject/Repositories/RedditRepository.cs
@@ -0,0 +1,41 @@
+﻿using RedditProject.Entities;
+using RedditProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditProject.Repositories
+{
+    public class RedditRepository
+    {
+        public RedditRepository(RedditContext redditContext)
+        {
+            RedditContext = redditContext;
+        }
+
+        public RedditContext RedditContext { get; set; }
+
+        public List<Reddit> PostList()
+        {
+            List<Reddit> Postlist = new List<Reddit>();
+
+            foreach (var item in RedditContext.Reddits)
+            {
+                Postlist.Add(item);
+            }
+
+            return Postlist;
+        }
+
+        public void AddPost(string content)
+        {
+            Reddit newPost = new Reddit()
+            {
+                Content = content
+            };
+            RedditContext.Add(newPost);
+            RedditContext.SaveChanges();
+        }
+    }
+}
