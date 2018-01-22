2c6edf610a47c628b0ba3c8641c5d193f978faff
Rikkerdaniel <rik.danka@gmail.com>
2018-01-22 14:28:34 +0100

    feat(rest):add sum till a endpoint

--- week-09/day-01/RestApifirstEx/RestApifirstEx/Controllers/HomeController.cs
+++ week-09/day-01/RestApifirstEx/RestApifirstEx/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestApifirstEx.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -27,6 +28,7 @@ namespace RestApifirstEx.Controllers
             }
             return Json(new { received = input, result = input * 2 });
         }
+
         [HttpGet("greeter")]
         public IActionResult Greeter(string name, string title)
         {
@@ -40,6 +42,7 @@ namespace RestApifirstEx.Controllers
             }
             return Json(new { welcome_message = "Oh, hi there " + name + ", my dear " + title + "!" });
         }
+
         [HttpGet("appenda/{appendable}")]
         public IActionResult AppendA(string appendable)
         {
@@ -49,5 +52,70 @@ namespace RestApifirstEx.Controllers
             }
             return Json(new { appended = appendable + "a" });
         }
+
+        [HttpPost("dountil/{what}")]
+        public IActionResult DoUntil([FromRoute]string what, [FromBody] Item input)
+        {
+            int sum = 0;
+            int fact = 1;
+            if (input.until == null)
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+            if (what == "sum" )
+            {
+                for (int i = 1; i < input.until + 1; i++)
+                {
+                    sum += i;
+                }
+                return Json(new { result = sum });
+            }
+            else if (what == "factor" )
+            {
+                for (int i = 1; i < input.until+1; i++)
+                {
+                    fact = fact * i;
+                }
+                return Json(new { result = fact });
+            }
+            else
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
+        }
+
     }
 }
+//        [HttpPost("dountil/{what}")]
+//        public IActionResult Dountil([FromRoute] string what, [FromBody] Item input)
+//        {
+//            if (input.until != null)
+//            {
+//                switch (what)
+//                {
+
+//                    case "sum":
+//                        int sum = 0;
+//                        for (int i = 1; i < input.until + 1; i++)
+//                        {
+//                            sum += i;
+//                        }
+//                        return Json(new { result = sum });
+
+//                    case "factor":
+//                        int factorial = 1;
+//                        for (int i = 1; i < input.until + 1; i++)
+//                        {
+//                            factorial *= i;
+//                        }
+//                        return Json(new { result = factorial });
+
+//                    default:
+//                        break;
+//                }
+//            }
+//            return Json(new { error = "Please provide a number!" });
+//        }
+//    }
+//}
+
---
+++ week-09/day-01/RestApifirstEx/RestApifirstEx/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApifirstEx.Models
+{
+    public class Item
+    {
+        public int? until { get; set; }
+    }
+}
