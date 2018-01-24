3694ac0c9bd35db3f7dd18aa70ffaa76f767ecbc
Rikkerdaniel <rik.danka@gmail.com>
2018-01-24 16:41:58 +0100

    feat(groot): refactor delet comment

--- week-09/day-03/IAmGroot/IAmGroot.IntegrationTests/UnitTest1.cs
+++ week-09/day-03/IAmGroot/IAmGroot.IntegrationTests/UnitTest1.cs
@@ -15,7 +15,6 @@ namespace IAmGroot.IntegrationTests
 
         public GuardianTests()
         {
-            //arrange
             Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             Client = Server.CreateClient();
         }
@@ -23,40 +22,30 @@ namespace IAmGroot.IntegrationTests
         [Fact]
         public async Task IndexShouldReturnNotOkStatus()
         {
-            //act
             var response = await Client.GetAsync("/");
-
-            //assert
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
 
         [Fact]
         public async Task GrootShouldReturnOkStatus()
         {
-            //act
             var response = await Client.GetAsync("/groot");
-
-            //assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         [Fact]
         public async Task GrootShouldReturnOKJson()
         {
-            //act
             var response = await Client.GetAsync("/groot?message=somemessage");
             string json = await response.Content.ReadAsStringAsync();
-            //assert
             Assert.Equal("{\"recived\":\"somemessage\",\"translated\":\"I am Groot!\"}", json);
         }
 
         [Fact]
         public async Task GrootShouldReturnErrorJson()
         {
-            //act
             var response = await Client.GetAsync("/groot?message=");
             string json = await response.Content.ReadAsStringAsync();
-            //assert
             Assert.Equal("{\"error\":\"I am Groot!\"}", json);
         }
 
