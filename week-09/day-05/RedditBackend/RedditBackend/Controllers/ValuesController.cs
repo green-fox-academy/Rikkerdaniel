577633442fec20d9fa0e852f3398ba313916f48c
Rikkerdaniel <rik.danka@gmail.com>
2018-01-26 08:14:24 +0100

    feat(reddit backend):backen base and some endpoints

---
+++ week-09/day-05/RedditBackend/RedditBackend/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedditBackend.Services;
+
+namespace RedditBackend.Controllers
+{
+    [Route("[controller]")]
+    public class ValuesController : Controller
+    {
+        public ValuesController(RedditService redditService)
+        {
+            RedditService = redditService;
+        }
+
+        public RedditService RedditService { get; set; }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var allPosts = RedditService.GetAllPosts();
+            return Json(new { posts = allPosts });
+        }
+
+        [HttpPost("")]
+        public IActionResult Create([FromBody] Post post)
+        {
+            RedditService.CreatePost(post);
+            return Json(RedditService.GetLastPost());
+        }
+
+    }
+}
---
+++ week-09/day-05/RedditBackend/RedditBackend/Services/RedditService.cs
@@ -0,0 +1,14 @@
+﻿using RedditBackend.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditBackend.Services
+{
+    public class RedditService
+    {
+       
+
+    }
+}
