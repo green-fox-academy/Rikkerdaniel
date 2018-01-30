326cee0bb740406058e43efc834d35c8c87d7934
Rikkerdaniel <rik.danka@gmail.com>
2018-01-30 10:15:41 +0100

    feat(foodorder):create context and dbsets

---
+++ project/FoodOrder/FoodOrder/Entities/FoodOrderContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using FoodOrder.Models;
+
+namespace FoodOrder.Entities
+{
+    public class FoodOrderContext : DbContext
+    {
+        public FoodOrderContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<MenuModel> MenuModels { get; set; }
+        public DbSet<OrderedFoodModel> MOrderedFoodModels { get; set; }
+        public DbSet<UserModel> UserModels { get; set; }
+    }
+}
--- project/FoodOrder/FoodOrder/Models/MenuModel.cs
+++ project/FoodOrder/FoodOrder/Models/MenuModel.cs
@@ -9,7 +9,6 @@ namespace FoodOrder.Models
     {
         public long Id { get; set; }
         public string Day { get; set; }
-        public string Type { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
     }
