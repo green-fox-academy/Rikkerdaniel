ba173ccb7b67e9d11fdc5cb521fd45d98374b7d0
Rikkerdaniel <rik.danka@gmail.com>
2018-01-30 14:29:36 +0100

    feat(foodorder):create a rest controller just to match the exercise

---
+++ project/FoodOrder/FoodOrder/Controllers/RestApiController.cs
@@ -0,0 +1,26 @@
+﻿using FoodOrder.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Controllers
+{
+    [Route("rest")]
+    public class RestApiController :Controller
+    {
+        public RestApiController(OrderFoodService orderFoodService)
+        {
+            OrderFoodService = orderFoodService;
+        }
+
+        public OrderFoodService OrderFoodService { get; set; }
+
+        [HttpGet("")]
+        public IActionResult RestThing()
+        {
+            return Json(OrderFoodService.OrderFoodRepository.ListOfOrders());
+        }
+    }
+}
